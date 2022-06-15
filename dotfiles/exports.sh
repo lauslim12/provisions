0decc358b44f00be4aaf63d5ed5e1a28ac6311f1
lauslim12 <31909304+lauslim12@users.noreply.github.com>
2022-06-15 22:13:56 +0700

    Allow environment variables to be placed in a dedicated dotfile

---
+++ dotfiles/exports.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+##
+# exports.sh
+#
+# Special dotfile used to place all of environment variables. This may be required when setting
+# up several applications where we need to place an additional `PATH` variable. To add a new
+# environment variable, you just need to do like the following: `echo "LANG=en_US.UTF-8" >> dotfiles/exports.sh`.
+##
+
+# Add your environment variables below.
