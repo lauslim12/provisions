c33d5206278cc4d03960b0e4658f2996a981b4e8
Nicholas <31909304+lauslim12@users.noreply.github.com>
2022-06-16 00:17:55 +0700

    Update functions.sh
    Adjust spacing in functions

--- dotfiles/functions.sh
+++ dotfiles/functions.sh
@@ -11,28 +11,28 @@
 # Initial setup of the dotfile, run essential functions before defining other, more generic functions.
 # Normalize `open` across Linux, macOS, and Windows. This is needed to make the `o` function (see below) cross-platform.
 if [ ! $(uname -s) = 'Darwin' ]; then
-	if grep -q Microsoft /proc/version; then
-		# Ubuntu on Windows using the Linux subsystem.
-		alias open='explorer.exe'
-	else
+  if grep -q Microsoft /proc/version; then
+    # Ubuntu on Windows using the Linux subsystem.
+    alias open='explorer.exe'
+  else
     # Normal open in Linux systems.
-		alias open='xdg-open'
-	fi
+    alias open='xdg-open'
+  fi
 fi
 
 # Determines the size of a file or a total size of a directory.
 fs() {
   if du -b /dev/null > /dev/null 2>&1; then
-		local arg=-sbh;
-	else
-		local arg=-sh;
-	fi
-	
+    local arg=-sbh;
+  else
+    local arg=-sh;
+  fi
+
   if [[ -n "$@" ]]; then
-		du $arg -- "$@"
-	else
-		du $arg .[^.]* ./*
-	fi;
+    du $arg -- "$@"
+  else
+    du $arg .[^.]* ./*
+  fi;
 }
 
 # Provides information about branches and remotes the Git repository.
@@ -87,11 +87,11 @@ nodeclean() {
 # `o` with no arguments opens the current directory, otherwise opens
 # the given location.
 o() {
-	if [ $# -eq 0 ]; then
-		open .
-	else
-		open "$@"
-	fi
+  if [ $# -eq 0 ]; then
+    open .
+  else
+    open "$@"
+  fi
 }
 
 # Switches to `Projects` folder with autocomplete for the subdirectories for quick
