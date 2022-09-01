e6badefdc0abb5bd08b6066f02ba7a24416a8ad1
lauslim12 <31909304+lauslim12@users.noreply.github.com>
2022-09-01 22:11:02 +0700

    Fix comments about default environment variables

--- dotfiles/exports.sh
+++ dotfiles/exports.sh
@@ -3,9 +3,8 @@
 ##
 # exports.sh
 #
-# Special dotfile used to place all of environment variables. This may be required when setting
-# up several applications where we need to place an additional `PATH` variable. To add a new
-# environment variable, you just need to do like the following: `echo "LANG=en_US.UTF-8" >> dotfiles/exports.sh`.
+# Special dotfile used to place all default environment variables. This may be required when setting
+# up several applications where we need to place an additional variables.
 ##
 
 # Add your environment variables below.
