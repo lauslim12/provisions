340ddbdc27a9384682769ab7f132d193013f691e
lauslim12 <31909304+lauslim12@users.noreply.github.com>
2024-04-02 22:39:24 +0900

    Simplify VSCode's `settings.json` and `extensions.sh`
    - For minimalism
    - Some configurations are not needed anymore: https://marketplace.visualstudio.com/items?itemName=dbaeumer.vscode-eslint
    - Remove almost all themes (except icons) as I'm using the default one anyways

--- dotfiles/vscode/extensions.sh
+++ dotfiles/vscode/extensions.sh
@@ -11,25 +11,16 @@
 # - To install all extensions: bash ./vscode/extensions.sh (make sure that you're in `provisions` folder).
 ##
 
-code --install-extension akamud.vscode-theme-onedark
-code --install-extension akamud.vscode-theme-onelight
-code --install-extension DavidAnson.vscode-markdownlint
+code --install-extension astro-build.astro-vscode
+code --install-extension davidanson.vscode-markdownlint
 code --install-extension dbaeumer.vscode-eslint
-code --install-extension dracula-theme.theme-dracula
-code --install-extension emmanuelbeziat.vscode-great-icons
 code --install-extension esbenp.prettier-vscode
 code --install-extension formulahendry.auto-rename-tag
-code --install-extension GitHub.github-vscode-theme
 code --install-extension golang.go
-code --install-extension Hyzeta.vscode-theme-github-light
-code --install-extension loilo.snazzy-light
 code --install-extension ms-python.black-formatter
+code --install-extension ms-python.debugpy
 code --install-extension ms-python.python
 code --install-extension ms-python.vscode-pylance
 code --install-extension naumovs.color-highlight
-code --install-extension PKief.material-icon-theme
-code --install-extension sdras.night-owl
-code --install-extension svelte.svelte-vscode
-code --install-extension teabyii.ayu
+code --install-extension pkief.material-icon-theme
 code --install-extension wayou.vscode-todo-highlight
-code --install-extension zhuangtongfa.material-theme
